2f041ea351c36f5e11afcd6ad372c62430732497
oculus-911 <lukepreddy@gmail.com>
2022-02-07 18:52:22 +0100

    refactored and tidied up a bit

--- scraping_imbalance_data.py
+++ scraping_imbalance_data.py
@@ -7,6 +7,55 @@ API_KEY ="96fmvyckhforx64"
 # todo this should be in a config file somewhere and get loaded at runtime
 #
 
+def get_yesterday_dt():
+  """Return a clean datetime object with yesterdays day"""
+
+  settlementDate_dt =datetime.datetime.utcnow() - datetime.timedelta(1)
+  settlementDate_dt = settlementDate_dt.replace(hour=0)
+  settlementDate_dt = settlementDate_dt.replace(minute=0)
+  settlementDate_dt = settlementDate_dt.replace(second=0)
+  settlementDate_dt = settlementDate_dt.replace(microsecond=0)
+  return settlementDate_dt
+
+def fetch_brcm_service_data_and_save( serviceNameList ,settlementDate,targetDirectory = "data/"):
+  """Takes a list of  services and a settlement date, fetches and stores them as csv files"""
+
+
+  # todo -1) make this target directory param,  if  the data already exists then dont bother fetching again
+  for serviceName in serviceNameList:
+    serviceDataBrms = BrmsApiWrapper(serviceName,settlementDate)
+    serviceDataList = serviceDataBrms.fetch()  # iterate over by period and return a list of lists with all the days data
+    smartestEnergyDataWrapperObject =SmartestEnergyDataWrapper()  # use this for reading and writing data
+    targetFilename = serviceName +"_" +settlementDate +".csv"
+    smartestEnergyDataWrapperObject.write_list_to_csv_file(serviceDataList,targetDirectory, targetFilename)
+
+  return
+
+def csv_data_to_merged_df(serviceNameList,settlementDate,targetDirectory = "data/"):
+  """This Fetches the csv data for given services and dates and returns a merged df with the prices and volumes"""
+  for serviceName in serviceNameList:
+    targetFilename = targetDirectory + serviceName + "_" + settlementDate + ".csv"
+    if serviceName == "ImbalancePrices":
+      imbalancePricesDf = None
+      imbalancePricesDf = pd.read_csv(targetFilename, usecols=["SettlementDate", "SettlementPeriod", "ImbalancePriceAmount", "PriceCategory"])
+      imbalancePricesDf = imbalancePricesDf[imbalancePricesDf.PriceCategory == "Excess balance"] # we need to condense this down as they duplciate the price data
+      imbalancePricesDf.set_index("SettlementPeriod")
+
+    if serviceName == "AggregatedImbalanceVolumes":
+      aggregatedImbalanceVolumesDf = None
+      aggregatedImbalanceVolumesDf = pd.read_csv(targetFilename,usecols=["SettlementDate", "SettlementPeriod", "ImbalanceQuantityMAW"])
+      aggregatedImbalanceVolumesDf.set_index('SettlementPeriod')
+
+  if imbalancePricesDf is not None and aggregatedImbalanceVolumesDf is not None:
+    mergedPricesVolumesDf = pd.merge(imbalancePricesDf, aggregatedImbalanceVolumesDf, left_on="SettlementPeriod",right_on="SettlementPeriod")
+    return mergedPricesVolumesDf
+  else:
+    raise ValueError("Missing csv data to construct dataframe")
+
+
+
+
+
 class PowerDfAnalytics:
   """Instantiate an analytics object that accepts a dataframe and contains analytics methods """
 
@@ -121,85 +170,35 @@ class BrmsApiWrapper:
 
           allPeriodsList.append(cleanedHeadersRow)
         dataRows = parseDataList[5:lastRow]
-
         for row in dataRows:
           row_list =row.split(",")
           allPeriodsList.append(row_list)
       except:
-        print(f"Failure for period {period_int}")
-
+        if period_int <49: # most days we only have 48 periods
+          print(f"Missing data for period {period_int}")
+        pass
     return allPeriodsList
 
 
+def main():
 
+  settlementDate_dt = get_yesterday_dt()
+  settlementDate  = datetime.datetime.strftime(settlementDate_dt, '%Y-%m-%d')
+  serviceNameList = ["ImbalancePrices" ,"AggregatedImbalanceVolumes"] # these are the services we want
+  fetch_brcm_service_data_and_save(serviceNameList,settlementDate)
+  mergedPricesVolumesDf =csv_data_to_merged_df(serviceNameList,settlementDate)
 
-# baseUrl = f"https://api.bmreports.com/BMRS/B1770/<VersionNo>?APIKey={self.apiKey}&SettlementDate={self.settlementDate}&Period={self.period}&ServiceType={self.serviceType}")
+  # now do the various analytics required
+  powerDfAnalytics = PowerDfAnalytics(mergedPricesVolumesDf)
+  maxHourlyPeriod = powerDfAnalytics.calculate_daily_abs_max_imbalance_volume_hour()
+  dailyImbalanceCost = powerDfAnalytics.calculate_sum_daily_imbalance_cost()
+  dailyImbalanceUnitRate = powerDfAnalytics.calculate_daily_imbalance_unit_rate()
 
-apiKey = API_KEY
+  settlementDateMaxHour_dt =  settlementDate_dt.replace(hour=maxHourlyPeriod) # for some nicer formatting on output
+  print(f"REPORTING: Total Daily Imbalance Cost is £{round(dailyImbalanceCost,2)} ")
+  print(f"REPORTING: Total Daily Imbalance Unit Rate   £{round(dailyImbalanceUnitRate,2)}/MWH")
+  print(f"REPORTING: Highest Absolute Volume imbalances at the hour of {settlementDateMaxHour_dt}")
 
-settlementDate_dt = datetime.datetime.utcnow() - datetime.timedelta(1)
-# clean this date format
-settlementDate_dt = settlementDate_dt.replace(hour=0)
-settlementDate_dt = settlementDate_dt.replace(minute=0)
-settlementDate_dt = settlementDate_dt.replace(second=0)
-settlementDate_dt = settlementDate_dt.replace(microsecond=0)
-settlementDate  = datetime.datetime.strftime(settlementDate_dt, '%Y-%m-%d')
-targetDirectory = "data/"
-
-# get imbalance data first
-serviceName = "ImbalancePrices"  # or "AggregatedImbalanceVolumes"
-print(f"Fetching {serviceName} data for {settlementDate}")
-serviceDataBrms = BrmsApiWrapper(serviceName,settlementDate)
-serviceDataList = serviceDataBrms.fetch()  # iterate over by period and return a list of lists with all the days data
-smartestEnergyDataWrapperObject =SmartestEnergyDataWrapper()  # use this for reading and writing data
-
-targetFilename = serviceName +"_" +settlementDate +".csv"
-smartestEnergyDataWrapperObject.write_list_to_csv_file(serviceDataList,targetDirectory, targetFilename)
-
-# get aggregatedImbalanceVolume
-
-serviceName = "AggregatedImbalanceVolumes"
-print(f"Fetching {serviceName} data")
-serviceDataBrms = BrmsApiWrapper(serviceName,settlementDate)
-serviceDataList = serviceDataBrms.fetch()  # iterate over by period and return a list of lists with all the days data
-smartestEnergyDataWrapperObject =SmartestEnergyDataWrapper()  # use this for reading and writing data
-
-targetFilename = serviceName +"_" +settlementDate +".csv"
-smartestEnergyDataWrapperObject.write_list_to_csv_file(serviceDataList,targetDirectory, targetFilename)
-print("Finished Fetching Raw Data")
-
-# read data from csv into a dataframe
-# todo - if  the data already exists then dont bother fetching again
-serviceName = "ImbalancePrices"  # or "AggregatedImbalanceVolumes"
-imbalancePricesFileName = targetDirectory + "/" + serviceName + "_" + settlementDate + ".csv"
-
-imbalancePricesDf = pd.read_csv(imbalancePricesFileName, usecols=["SettlementDate", "SettlementPeriod", "ImbalancePriceAmount","PriceCategory"])
-# we need to condense this down as they duplciate the price data
-condensedImbalancePricesDf = imbalancePricesDf[imbalancePricesDf.PriceCategory== "Excess balance"]
-condensedImbalancePricesDf.set_index("SettlementPeriod")
-
-
-
-serviceName = "AggregatedImbalanceVolumes"
-aggregatedImbalanceVolumesFileName = targetDirectory + "/" + serviceName + "_" + settlementDate + ".csv"
-aggregatedImbalanceVolumesDf = pd.read_csv(aggregatedImbalanceVolumesFileName, usecols=["SettlementDate", "SettlementPeriod", "ImbalanceQuantityMAW"])
-aggregatedImbalanceVolumesDf.set_index('SettlementPeriod')
-# mergedPricesVolumesDf =  imbalancePricesDf.merge(aggregatedImbalanceVolumesDf, left_index=True,right_index=True)
-# mergedPricesVolumesDf =  condensedImbalancePricesDf.merge(aggregatedImbalanceVolumesDf, left_index=True,right_index=True)
-mergedPricesVolumesDf =  pd.merge(condensedImbalancePricesDf, aggregatedImbalanceVolumesDf , left_on="SettlementPeriod", right_on="SettlementPeriod")
-print("finished merging dataframe")
-
-# ok now do the various analytics required
-powerDfAnalytics = PowerDfAnalytics(mergedPricesVolumesDf)
-maxHourlyPeriod = powerDfAnalytics.calculate_daily_abs_max_imbalance_volume_hour()
-dailyImbalanceCost = powerDfAnalytics.calculate_sum_daily_imbalance_cost()
-dailyImbalanceUnitRate = powerDfAnalytics.calculate_daily_imbalance_unit_rate()
-
-
-settlementDateMaxHour_dt =  settlementDate_dt.replace(hour=maxHourlyPeriod)
-
-print(f"REPORTING: Total Daily Imbalance Cost is £{round(dailyImbalanceCost,2)} ")
-print(f"REPORTING: Total Daily Imbalance Unit Rate   £{round(dailyImbalanceUnitRate,2)}/MWH")
-print(f"REPORTING: Highest Absolute Volume imbalances were the hour of {settlementDateMaxHour_dt}")
-print("Finished")
-#todo refactor
+if __name__ == "__main__":
+  # entrypoint
+  main()
